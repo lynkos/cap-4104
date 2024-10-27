df38dca8c14b6aeaf2ad61300267a465e878b4bb
Kiran Brahmatewari <kiwi2mii@gmail.com>
2024-10-27 19:58:59 -0400

    Remove menubar files

--- js/menubar.js
+++
@@ -1,6 +0,0 @@
-fetch("menubar.html") 
-.then(response => response.text()) 
-.then(data => { 
-    document.getElementById("menubar").innerHTML = data; 
-}) 
-.catch(error => console.error("Error loading menubar:", error)); 
