59e7d170c7653d6ce5c8c502e7fd296a7035f166
Kiran Brahmatewari <kiwi2mii@gmail.com>
2024-10-27 20:03:14 -0400

    Make stars background transparent (in order to overlay onto another gradient background)

--- js/stars.js
+++ js/stars.js
@@ -7,6 +7,6 @@ const starback = new Starback(canvas, {
   starColor: "#fff",
   quantity: 100,
   direction: 225,
-  backgroundColor: [ "#583b7a", "#643f81", "#714489", "#7e488f", "#8b4c96", "#93539e", "#9a5ba7", "#a262af", "#a56ebb", "#a97bc7", "#ac87d2", "#b093dd" ],
+  backgroundColor: "transparent", // [ "#583b7a", "#643f81", "#714489", "#7e488f", "#8b4c96", "#93539e", "#9a5ba7", "#a262af", "#a56ebb", "#a97bc7", "#ac87d2", "#b093dd" ],
   randomOpacity: true,
 });
